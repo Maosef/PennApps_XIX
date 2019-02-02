c6744dadb403ddbe5a368990ebe3675049e1032d
mazeiomli <mazeiomli@gmail.com>
2019-02-02 15:33:27 -0500

    Using urllib.request now instead of requests

--- bookshelf/scrapeID.py
+++ bookshelf/scrapeID.py
@@ -6,14 +6,14 @@ Scrapes the book ID of a book from Gutenberg
 '''
 
 from bs4 import BeautifulSoup
-import requests
+from urllib.request import urlopen
 
 def main():
     #Start here
     #URL here
     url = "http://www.gutenberg.org/ebooks/search/?query=pride+and+prejudice"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    page = urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
     firstBook = soup.find('a', attrs={'accesskey':'3'});
     firstBookHref = firstBook['href']
     # firstBookHref is a string '/ebooks/XXXX'
