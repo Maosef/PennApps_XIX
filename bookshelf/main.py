6d24be1db2e41c8a3249d9bf15d86ca769f48a5a
Maosef <pazzun@gmail.com>
2019-02-03 10:45:26 -0500

    done

--- bookshelf/main.py
+++ bookshelf/main.py
@@ -1,7 +1,8 @@
 
 from gcloud_ocr import detect_text
 from isbn_google_books import get_isbn_10
-from rec_books import get_related
+from scrape_goodreads import get_goodreads_id 
+from rec_books import get_recs
 
 path = "books_rotated.jpg"
 
@@ -11,16 +12,12 @@ texts = texts[0].description
 
 texts = texts.split('\n')
 
-#filter bad titles
-#get ISBN number and book contents
+#filter bad titles, get related books from Goodreads
 for title in texts:
     #print(title)
-    if not all(ord(c) < 128 for c in title): continue
-
-    isbn = get_isbn_10(title)
-    print(title, isbn)
-    rec_list = get_related(isbn)
-    if rec_list == None: continue    
+    work_id = get_goodreads_id(title) 
+    rec_list = get_recs(str(work_id))
+    if rec_list == -1 or rec_list == None: continue    
     for rec in rec_list:
         print(rec)
 
