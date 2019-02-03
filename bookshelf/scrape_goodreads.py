6a0894e9c802fdc0731dbbaf329f6bf2f3e9cffc
mazeiomli <mazeiomli@gmail.com>
2019-02-03 10:33:40 -0500

    Get book ID from GoodReads

--- bookshelf/scrape_goodreads.py
+++ bookshelf/scrape_goodreads.py
@@ -1,6 +1,34 @@
+'''
+Given title, make API call to GoodReads and
+return the work ID.
+
+Returns 0 if not found
+'''
+import xml.etree.ElementTree as ET
+import requests
 
 
-def query(query):
-    pass
 
-def get re
+def get_goodreads_id(title: str) -> int:
+    id = 0
+
+    baseURL = r'https://www.goodreads.com/search.xml?key=vfjQv3RyzRKtbRfFjX7Q&q='
+    titleSplit = title.split(' ')
+    formattedTitle = '+'.join(titleSplit)
+    url = baseURL + formattedTitle
+
+    try:
+        response = requests.get(url)
+    
+        tree = ET.fromstring(response.content)
+        id_element = tree.find(r'./search/results/work/id')
+        id_string = ET.tostring(id_element, encoding='utf-8').decode('utf-8')
+        first_num = id_string.find('>') + 1
+        last_num = id_string.find('<', 1)
+        id = int(id_string[first_num: last_num])
+        
+    except:
+        # Handle error
+        pass
+    
+    return id
